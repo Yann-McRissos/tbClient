1f47117cc53ba02a4f124a96136ffe0ef7e0467d
furest <yannick.diepart@hotmail.com>
2019-08-18 01:32:28 +0200

    Update PacketBridge.sh

--- bin/PacketBridge.sh
+++ bin/PacketBridge.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 java -jar /etc/tbClient/bin/PacketBridge.jar >/tmp/packetbridgeout 2>&1 $@ &
 sleep 3
-if [[ `grep -c "No route to host\|Connection refused" /tmp/packetbridgeout`  != 0 ]]; then
+if [[ `grep -c "No route to host" /tmp/packetbridgeout`  != 0 ]]; then
 	echo "firewallerror"
+fi
+if [[ `grep -c "Connection refused" /tmp/packetbridgeout`  != 0 ]]; then
+	echo "nopterror"
 fi
 rm /tmp/packetbridgeout
 echo $!
