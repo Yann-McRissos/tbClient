fa7996562b34d4a4f00750771b34d886fc52de43
Yann-McRissos <citrouillon@skynet.be>
2021-11-30 16:57:45 +0100

    + start time + print time
    :sparkles: Lab now starts at the right time.
    :sparkles: Logs are now timestamped.

--- bin/main.py
+++ bin/main.py
@@ -7,14 +7,13 @@ from connect import *
 
 # State vars
 LAB = 0
-
 def thr_lab_timer(name, labDuration):
     global LAB
-    print("Thread " + name + ": starting", )
-    print("Duration of lab is " + labDuration + " seconds ("+labDuration/3600+" hours)")
+    print(datetime.now().strftime("%m/%d-%H:%M:%S") + ": Thread " + name + ": starting", )
+    print(datetime.now().strftime("%m/%d-%H:%M:%S") + ": Duration of lab is " + labDuration + " seconds ("+labDuration/3600+" hours)")
     time.sleep(labDuration)
     LAB = 0
-    print("Thread " + name + ": finishing")
+    print(datetime.now().strftime("%m/%d-%H:%M:%S") + ": Thread " + name + ": finishing")
 
 
 if __name__ == "__main__":
@@ -28,12 +27,15 @@ if __name__ == "__main__":
                 peer = getPeering()
                 if peer == -1 or peer == 404:
                     if peer == 404:
-                        print("Error: Got 404 from the server, is the address correct ?")
+                        print(datetime.now().strftime("%m/%d-%H:%M:%S") + ": Error: Got 404 from the server, is the address correct ?")
                     else:
-                        print("Error while retrieving peer information.")
-                    print("Retrying in 5 minutes")
+                        print(datetime.now().strftime("%m/%d-%H:%M:%S") + ": Error while retrieving peer information.")
+                    print(datetime.now().strftime("%m/%d-%H:%M:%S") + ": Retrying in 5 minutes")
                     time.sleep(300) # Sleep for 5 minutes
                 else:
+                    print(datetime.now().strftime("%m/%d-%H:%M:%S") + ": Lab starts on " + peer["start_date"].strftime("%d/%m/%Y @ %H:%M:%S"))
+                    while(datetime.now() < peer["start_date"]):
+                        sleep(60)
                     connect(peer)
                     LAB = 1
                     # daemonize the thread so that it stops with the main program
@@ -44,6 +46,6 @@ if __name__ == "__main__":
                     # Flush interfaces
                     rc = call("./flush.sh")
             except Exception as e:
-                print("Error : Could not retrieve peer information. Retrying in 5 minutes.")
-                print("Error message: " + str(e))
+                print(datetime.now().strftime("%m/%d-%H:%M:%S") + ": Error : Could not retrieve peer information. Retrying in 5 minutes.")
+                print(datetime.now().strftime("%m/%d-%H:%M:%S") + ": Error message: " + str(e))
                 time.sleep(300) # Sleep for 5 minutes
